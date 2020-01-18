d12d83091b9eea3379582e5007528f593d6630bb
Chris <cbaucom@business.com>
2020-01-17 22:07:54 -0500

    Remove unused stylesheets

--- src/components/layout.js
+++ src/components/layout.js
@@ -34,16 +34,6 @@ const Layout = ({ children }) => {
         ]}
       >
         <html lang="en" />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        />
       </Helmet>
       <LayoutContainer id="App">
         <GlobalStyle />
