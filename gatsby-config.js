22f87cd468a5d5dcf73ed4d73bef820fd154a7a3
CB <chrisrb83@gmail.com>
2021-01-01 17:36:08 -0600

    Add gatsby-plugin-react-helmet-canonical-url

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,12 @@ module.exports = {
 	plugins: [
 		`gatsby-plugin-react-helmet`,
 		{
+			resolve: `gatsby-plugin-react-helmet-canonical-urls`,
+			options: {
+				siteUrl: `https://www.perspectivgardens.com`,
+			},
+		},
+		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
 				name: `images`,
