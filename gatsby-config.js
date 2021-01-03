0eb8edcb97b95d937ed6fc98c566dc450b368d51
CB <chrisrb83@gmail.com>
2021-01-03 11:10:09 -0600

    Replace google drive with cloudinary

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,11 +40,14 @@ module.exports = {
 			},
 		},
 		{
-			resolve: `gatsby-source-drive`,
+			resolve: `gatsby-source-cloudinary`,
 			options: {
-				folderId: process.env.GOOGLE_DRIVE_FOLDER_ID,
-				key: process.env.GOOGLE_KEY,
-				serviceAccountEmail: process.env.GOOGLE_SA_EMAIL,
+				cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+				apiKey: process.env.CLOUDINARY_API_KEY,
+				apiSecret: process.env.CLOUDINARY_API_SECRET,
+				resourceType: `image`,
+				prefix: `perspectivgardens.com/`,
+				maxResults: 100,
 			},
 		},
 		{
